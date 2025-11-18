799487d3e1d78fa13cbbf080f3adc9f9b1a75dee
seokjun_kim <kimsj6978@gmail.com>
2025-11-18 21:44:51 +0900

    기초 세팅 수정

--- zero/settings.py
+++ zero/settings.py
@@ -142,4 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
-CORS-ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    'http"//프론트엔드주소',
+    'http"//프론트엔드주소',
+]
