6c6175bc158b935a6dabeff61cd9e05077f949be
jenajeong <skg4660@gmail.com>
2025-11-21 21:54:40 +0900

    [FEAT] DB에 대한 admin 생성

--- create/admin.py
+++ create/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Song, SongDetail, Word, History, Playlist
 
-# Register your models here.
+admin.site.register(Song)
+admin.site.register(SongDetail)
+admin.site.register(Word)
+admin.site.register(History)
+admin.site.register(Playlist)
