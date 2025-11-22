4710a89c83ade5366c69d33afa381836a0008f0e
seokjun_kim <kimsj6978@gmail.com>
2025-11-22 14:29:19 +0900

    Revert "Revert "Feat: 콜백 엔드포인트 구현""

--- create/urls.py
+++ create/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import CreateSongView, MeaningView, SunoGenerateView
+from .views import CreateSongView, MeaningView, SunoGenerateView, SunoCallbackView
 
 urlpatterns = [
     path("", CreateSongView.as_view()),
     path("song/", CreateSongView.as_view(), name="create-song"),
     path("meaning/<str:word>/", MeaningView.as_view(), name="create-meaning"),
+
+    # Suno AI 콜백 엔드포인트
+    # 최종 URL: /api/create/callback/
+    path("callback/", SunoCallbackView.as_view(), name="suno-callback"),
 
     # Suno AI 를 이용한 곡 생성
     # 최종 URL: /api/create/<song_id>/
